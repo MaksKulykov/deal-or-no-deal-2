1e534ab85625b81cb5b9b9a620b3da9a12c6bfbb
maksim.k.as@agiliumlabs.com <kulykov.maks@gmail.com>
2022-04-06 14:42:33 +0200

    implemented unit-tests for layout component

---
+++ src/components/layout.test.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Layout from './layout';
+
+describe('Layout component', () => {
+
+    let children: null;
+
+    test('should display layout', () => {
+        render(<Layout children={children}/>);
+        expect(screen.getByTestId('layout-wrapper')).toBeVisible();
+    });
+
+});
